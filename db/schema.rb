6c90608c057315271667d37424dfb5478267fadd
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-09-16 15:58:22 -0300

    scaffold course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_155124) do
+ActiveRecord::Schema.define(version: 2021_09_16_184431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
