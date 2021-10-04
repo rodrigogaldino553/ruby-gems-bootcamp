0198f0f2f18618c58a69722a3f580d7272204430
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-10-04 12:45:44 -0300

    add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
+
+  rolify
+
   def to_s
     email
   end
@@ -13,4 +16,17 @@ class User < ApplicationRecord
   end
 
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher)
+    end
+  end
 end
