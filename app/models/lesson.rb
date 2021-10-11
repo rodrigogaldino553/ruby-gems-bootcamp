7e7647caeb647476ddaaf240cd7f56ddda33a6e7
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-10-11 13:07:15 -0300

    add lessons activity tracker

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,12 @@ class Lesson < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    title
+  end
+  
 end
