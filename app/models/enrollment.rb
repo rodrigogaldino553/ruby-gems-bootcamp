112b69491a451bb8ebcce51f33896376c82bce4a
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-11-06 18:20:16 -0300

    g scaffold enrollments --working

---
+++ app/models/enrollment.rb
@@ -0,0 +1,19 @@
+class Enrollment < ApplicationRecord
+  belongs_to :course
+  belongs_to :user
+
+  validates :user, :course, presence: true
+  
+  validates_uniqueness_of :user_id, scope: :course_id
+  validates_uniqueness_of :course_id, scope: :user_id
+
+  validate :cant_subscribe_to_own_course
+  protected
+  def cant_subscribe_to_own_course
+    if self.new_record? && user_id.present? && (self.user_id == course.user_id)
+      errors.add(:base, 'You cant subcribe to your own course!')
+    end
+  end
+  
+  
+end
