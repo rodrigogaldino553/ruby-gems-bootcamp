8793113ebdab43112dad10e820fa81f40c9c9d5c
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-11-06 18:40:23 -0300

    styling enrollments pages

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -8,12 +8,16 @@ class Enrollment < ApplicationRecord
   validates_uniqueness_of :course_id, scope: :user_id
 
   validate :cant_subscribe_to_own_course
+
+  def to_s
+    user.to_s + " " + course.to_s
+  end
+  
   protected
   def cant_subscribe_to_own_course
     if self.new_record? && user_id.present? && (self.user_id == course.user_id)
       errors.add(:base, 'You cant subcribe to your own course!')
     end
   end
-  
-  
+
 end
