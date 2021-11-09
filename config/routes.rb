b8cdd11802c796d7e88c44190e6101d12c7ea4be
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-11-09 12:44:14 -0300

    add system to buy courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :enrollments
   devise_for :users
 
   resources :courses do
     resources :lessons
+    resources :enrollments, only: [:new, :create]
   end
   
   resources :users, only: [:index, :edit, :show, :update]
