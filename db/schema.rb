842429f6ec4c924ef03cbcacd8bd577529183387
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-01-23 22:22:45 -0300

    add friendly_id for enrollments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_181240) do
+ActiveRecord::Schema.define(version: 2022_01_24_010818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,9 @@ ActiveRecord::Schema.define(version: 2021_11_06_181240) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["slug"], name: "index_enrollments_on_slug", unique: true
     t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
