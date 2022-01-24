40e9752ee0f54ede3a1d0d4c0477f0cf92470055
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-01-23 22:53:35 -0300

    add ransack and filters to enrollments index

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -4,8 +4,12 @@ class EnrollmentsController < ApplicationController
 
   # GET /enrollments or /enrollments.json
   def index
-    @enrollments = Enrollment.all
-    @pagy, @enrollments = pagy(Enrollment.all)
+    #@enrollments = Enrollment.all
+    #@pagy, @enrollments = pagy(Enrollment.all)
+
+    @q = Enrollment.ransack(params[:q])
+    @pagy, @enrollments = pagy(@q.result.includes(:user))
+
     authorize @enrollments
   end
 
