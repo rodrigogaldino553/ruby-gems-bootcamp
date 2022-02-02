b1669292d02a4da564cb94063fe443b9fdc9cf69
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-02-02 12:58:21 -0300

    organize routes in namespace block

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "activity", to: 'home#activity'
   get "analytics", to: 'home#analytics'
-  get "charts/users_per_day", to: 'charts#users_per_day'
-  get "charts/enrollments_per_day", to: 'charts#enrollments_per_day'
-  get "charts/course_popularity", to: 'charts#course_popularity'
-
+  #get "charts/users_per_day", to: 'charts#users_per_day'
+  #get "charts/enrollments_per_day", to: 'charts#enrollments_per_day'
+  #get "charts/course_popularity", to: 'charts#course_popularity'
+  #get "charts/money_makers", to: 'charts#money_makers'
+  
+  namespace :charts do
+    get "users_per_day"
+    get "enrollments_per_day"
+    get "course_popularity"
+    get "money_makers"
+  end
   root "home#index"
 end
