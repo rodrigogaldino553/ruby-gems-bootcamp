8e8f1e1a665c2bb13431b2f33b9b1a885b1321e7
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-02-05 11:50:54 -0300

    add complex validation to course show

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -5,6 +5,10 @@ class CoursePolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    @record.published && @record.approved || @user.present? && @user.has_role(:admin) || @user.present? && @record.user_id == @user.id || @record.bought(@user)
+  end
+
   def edit?
     @record.user == @user
     # @record.user_id == @user.id || @user.has_role?(:admin)
