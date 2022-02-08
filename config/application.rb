fc673342c0bdbbd2686ee19aaa9fca7b02306f8d
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-02-08 20:48:01 -0300

    add rails-erd gem to genrate relationship diagrams

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RubyGemsBootcamp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    if Rails.env.development? # for rails-erd to generate diagrams
+      def eager_load!
+        Zeitwerk::Loader.eager_load_all
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
