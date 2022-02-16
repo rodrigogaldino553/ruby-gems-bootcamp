4e0c6af64d184f8f861bcde388d33af783197492
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-02-16 16:37:56 -0300

    rank courses lessons

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -65,7 +65,7 @@ class CoursesController < ApplicationController
   # GET /courses/1 or /courses/1.json
   def show
     authorize @course
-    @lessons = @course.lessons
+    @lessons = @course.lessons.rank(:row_order)
     @enrollments_with_review = @course.enrollments.reviewed
   end
 
