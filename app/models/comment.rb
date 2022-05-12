622535475e740e946bc36718d3ead725ad05bb4c
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-05-12 07:04:45 -0300

    feat: public activity to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ApplicationRecord
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
   validates :content, presence: true
+
+  def to_s
+    self.content
+  end
 end
