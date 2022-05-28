0504e594b418e2e47644929663009e18bca5cb14
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-05-28 15:26:57 -0300

    add privacy policy to the app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, :only => [:index]
+  skip_before_action :authenticate_user!, :only => [:index, :privacy_policy]
 
   def index
     @courses = Course.all.limit(3)
@@ -26,5 +26,8 @@ class HomeController < ApplicationController
     else
       redirect_to root_path, alert: 'You are not authorize to access this page!'
     end
+  end
+
+  def privacy_policy
   end
 end
