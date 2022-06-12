f15366ce70c562f524095c4412bc1b43fd000d46
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-06-12 10:36:09 -0300

    fix sign in with google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, Rails.application.credentials.dig(:google_oauth, :app_id), Rails.application.credentials.dig(:google_oauth, :app_secret), {}
-  OmniAuth.config.allowed_request_methods = %i[get]
+  OmniAuth.config.allowed_request_methods = %i[post get]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
