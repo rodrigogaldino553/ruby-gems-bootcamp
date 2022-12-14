ea76ec55c8aef7440d81214d607342404f9fb12a
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-12-14 13:22:05 -0300

    add filter by tags

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,35 +14,40 @@ class CoursesController < ApplicationController
 
     @ransack_courses = Course.published.approved.ransack(params[:courses_search], search_key: :courses_search)
     @courses = @ransack_courses.result.includes(:user)
-    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
+    @tags = Tag.all
     # end
   end
 
   def purchased
     @ransack_path = purchased_courses_path
     @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
-    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
+    @tags = Tag.all
     render 'index'
   end
 
   def pending_review
     @ransack_path = pending_review_courses_path
     @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
-    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
+    @tags = Tag.all
     render 'index'
   end
 
   def created
     @ransack_path = created_courses_path
     @ransack_courses = Course.where({user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
-    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
+    @tags = Tag.all
     render 'index'
   end
 
   def unapproved
     @ransack_path = unapproved_courses_path
     @ransack_courses = Course.unapproved.ransack(params[:courses_search], search_key: :courses_search)
-    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
+    @tags = Tag.all
     render 'index'
   end
 
@@ -73,11 +78,13 @@ class CoursesController < ApplicationController
   def new
     @course = Course.new
     authorize @course
+    @tags = Tag.all
   end
 
   # GET /courses/1/edit
   def edit
     authorize @course
+    @tags = Tag.all
   end
 
   # POST /courses or /courses.json
@@ -91,6 +98,7 @@ class CoursesController < ApplicationController
         format.html { redirect_to @course, notice: "Course was successfully created." }
         format.json { render :show, status: :created, location: @course }
       else
+        @tags = Tag.all
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
@@ -105,6 +113,7 @@ class CoursesController < ApplicationController
         format.html { redirect_to @course, notice: "Course was successfully updated." }
         format.json { render :show, status: :ok, location: @course }
       else
+        @tags = Tag.all
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
