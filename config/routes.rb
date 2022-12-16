a7a6b17d98907c4e35566e29becba76d67ba14ac
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-12-16 10:10:27 -0300

    create new tags through selectize view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get :my_students, on: :collection
   end
 
+  resources :tags, only: :create
   resources :courses do
     get :purchased, :pending_review, :created, :unapproved, on: :collection
     member do
