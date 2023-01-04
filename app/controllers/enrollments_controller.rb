29b8ccf0ecceb49f932cb22c931ef157443c290d
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-01-04 15:58:42 -0300

    create authorization for course certificate

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -23,13 +23,14 @@ class EnrollmentsController < ApplicationController
   end
 
   def certificate
+    authorize @enrollment, :certificate?
     respond_to do |format|
       format.html
       format.pdf do
         # render pdf: "file_name"   # Excluding ".pdf" extension.
         render pdf: "#{@enrollment.course.title}, #{@enrollment.user.email}",
         page_size: 'A4',
-        template: 'enrollments/show.pdf.haml'
+        template: 'enrollments/certificate.pdf.haml'
       end
     end
   end
